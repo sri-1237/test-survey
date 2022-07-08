4ba4d1998942a72b919acb141617b85c61fa0494
sri-1237 <srilakshmi.sadanala@eagles.oc.edu>
2022-07-08 18:52:35 -0500

    sample code

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="test-survey-builder">
+    <h2 class="text-center">Vue Survey Builder Demo</h2>
+    <hr/>
+    <QuestionsView :questions="questionsList" :readOnly="true" />
+    <div v-if="addQuestion">
+      <SurveyBuilder :options="sampleQuestion" @add-update-question="updateQuestionsList"/>
+    </div>
+    <div class="pt-10">
+      <button type="button" class="add_another_btn br-25" v-on:click="addNewQuestion()">Add question</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SurveyBuilder from './components/SurveyBuilder.vue';
+import QuestionsView from './components/QuestionView.vue';
+import sampleQuestionObj from './components/survey-builder.json';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'TestSurveyBuilder',
+  data() {
+    return {
+      questionsList: [],
+      addQuestion: false,
+    };
+  },
+  mounted() {
+    // this.$root.$on('add-update-question', q => {
+    //   this.updateQuestionsList(q);
+    // });
+  },
+  components: { SurveyBuilder, QuestionsView },
+  methods: {
+    updateQuestionsList(q) {
+      console.log("qqq..",q.question);
+      var question = q.question;
+      const questionIndex = this.questionsList.findIndex(x => x.id === question.id);
+      console.log("q index..",questionIndex);
+      if (questionIndex >= 0) {
+        this.questionsList.splice(questionIndex, 1, question);
+      } else {
+        this.questionsList.push(JSON.parse(JSON.stringify(question)));
+      }
+      this.addQuestion = false;
+      this.$emit('selected-question', null);
+      console.log("q list", JSON.stringify(this.questionsList))
+      window.console.log(question, this.addQuestion, this.questionsList);
+    },
+    addNewQuestion() {
+      this.sampleQuestion = JSON.parse(JSON.stringify(sampleQuestionObj));
+      console.log("111111",this.sampleQuestion)
+      this.addQuestion = true;
+    },
+  },
+};
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +67,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.add_another_btn {
+  font-size: 14px;
+  background-color: transparent;
+  border-color: #4c8ce4;
+  color: #4c8ce4;
+  padding: 8px;
+  cursor: pointer;
+}
+.text-center {
+  text-align: center;
+}
 </style>
+
